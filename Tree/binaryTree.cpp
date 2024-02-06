8e847df966d768add17930c75ee8ab72ccdab06e
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2024-02-06 09:41:51 +0600

    Build Binary tree with Preorder

---
+++ Tree/binaryTree.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+// Input : 1 2 4 -1 -1 5 7 -1 -1 -1 3 -1 6 -1 -1
+
+class Node {
+    public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int d){
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node* buildTree(){
+    int d;
+    cin>>d;
+    if(d==-1){
+        return NULL;
+    }
+    Node* n = new Node(d);
+    n->left = buildTree();
+    n->right = buildTree();
+    return n;
+}
+
+int main(){
+    cout<<"Hello Tree";
+    return 0;
+}
